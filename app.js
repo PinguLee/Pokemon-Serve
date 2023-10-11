52fd77651021d24ed4c19255f4ea3f51a578515c
Pingu Lee <137541277+PinguLee@users.noreply.github.com>
2023-10-11 09:55:21 +0900

    test

--- app.js
+++ app.js
@@ -1,40 +1,28 @@
+// 내장모듈 불러오기
 const http = require('http');
-const server = http.createServer(function (request, response) {
-  console.log(request.url);
-  if (request.url === "/") {
-    let main = `<!DOCTYPE html>
-    <html lang="ko">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-    </head>
-    <body>
-      <h1>main</h1>
-      <a href="http://localhost:1234/sub">sub</a>
-    </body>
-    </html>`
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(main);
+// 외부모듈 불러오기(내가 만든 모듈)
+const HTMLTemplate = require('./model/HTML-model');
+
+const contentType = {
+  'Content-Type': 'text/html',
+  'charset': 'utf-8',
+}
+
+// 서버 생성
+const server = http.createServer(function(request, response){
+
+  if(request.method === 'GET' && request.url === '/'){
+    // 200 코드는 정상적으로 응답했다는 의미이다.
+    response.writeHead(200, contentType);
+    response.end(HTMLTemplate);
+  } else{
+    // 404 코드와 함께 페이지를 찾을 수 없음을 알려주는 응답을 보낸다.
+    response.writeHead(404, contentType);
+    response.end('<h1>페이지 요청데이터를 찾을 수 없을 때 404에러를 응답하는 것이 HTTP 프로토콜</h1>');
   }
-  if (request.url == "/sub") {
-    let sub = `<!DOCTYPE html>
-    <html lang="ko">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-    </head>
-    <body>
-      <h1>sub</h1>
-      <a href="http://localhost:1234/">main</a>
-    </body>
-    </html>`
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(sub);
-  }
+
 });
 
-server.listen(1234);
+server.listen(3000, function(){
+  console.log('서버가 가동중입니다. 끄려면 Ctrl + C를 누르세요.');
+});
